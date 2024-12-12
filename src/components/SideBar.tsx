1ae52e5a59948b7d499f0fb0c06d49fdaec48faf
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-12-12 16:00:08 -0500

    feat(sidebar 2/4)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,13 +1,21 @@
 export default function SideBar() {
   return (
-    <div className="flex h-screen w-64 flex-col justify-between bg-gray-800 p-6 text-white">
+    <div className="flex h-screen w-auto flex-col bg-gray-800 p-6 text-white">
       <div>
         <h2 className="mb-6 text-2xl font-bold">POS System</h2>
       </div>
+      <div></div>
+      <div>Lista de productos y botones</div>
       <div>
-        <button className="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600">
-          Vender
-        </button>
+        <div></div>
+        <div className="space-y-3">
+          <button className="w-full rounded bg-green-500 px-4 py-2 font-bold uppercase text-white hover:bg-green-600">
+            Vender <span>$ {} total</span>
+          </button>
+          <button className="w-full rounded bg-red-500 px-4 py-2 font-bold uppercase text-white hover:bg-red-600">
+            Cancelar Pedido
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/Pos.tsx
+++ src/pages/Pos.tsx
@@ -3,6 +3,7 @@ import { Product } from "../types";
 import SideBar from "../components/SideBar";
 import { productos } from "../data/products";
 import { toast } from "react-toastify";
+import CardProduct from "../components/CardProduct";
 
 export const Pos = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -19,62 +20,51 @@ export const Pos = () => {
   //   fetchProducts();
   // }, []);
 
-  // const hanglePos = () => {};
+  const onAddToCart = () => {};
 
   useEffect(() => {
     setProducts(productos);
   }, []);
 
   return (
-    <div className="flex">
+    <div className="flex max-md:flex-col">
       <div className="flex-1 p-6">
         <div className="flex justify-between">
           <h1 className="mb-6 text-2xl font-bold">Products</h1>
           <div>
             <button
-              className="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+              className="mx-4 rounded-3xl bg-green-500 px-4 py-2 text-white hover:bg-green-600"
               onClick={() => {
                 toast.success(`Product to POS`);
               }}
-            />
+            >
+              Texto de Ejemplo
+            </button>
             <button
-              className="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+              className="mx-4 rounded-3xl bg-green-500 px-4 py-2 text-white hover:bg-green-600"
               onClick={() => {
                 toast.success(`Product to POS`);
               }}
-            />
+            >
+              Texto de Ejemplo
+            </button>
             <button
-              className="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
+              className="mx-4 rounded-3xl bg-green-500 px-4 py-2 text-white hover:bg-green-600"
               onClick={() => {
                 toast.success(`Product to POS`);
               }}
-            />
+            >
+              Texto de Ejemplo
+            </button>
           </div>
         </div>
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <div
-              key={product.id}
-              className="overflow-hidden rounded-md bg-white shadow-md"
-            >
-              <img
-                src={product.image}
-                alt={product.name}
-                className="h-40 w-full object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-lg font-bold">{product.name}</h3>
-                <p className="text-gray-600">Price: ${product.price}</p>
-                <button
-                  className="mt-2 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
-                  onClick={() => {
-                    toast.success(`Product ${product.name} to POS`);
-                  }}
-                >
-                  Add to
-                </button>
-              </div>
-            </div>
+            <CardProduct
+              key={product._id}
+              product={product}
+              onAddToCart={onAddToCart}
+            />
           ))}
         </div>
       </div>
