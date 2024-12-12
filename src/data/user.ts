0614a86ab264cd7e6585047e6563e430df6730d5
Evelyn R <evelynrodriguezc0@gmail.com>
2024-12-11 21:28:08 -0500

    adding user elements

---
+++ src/data/user.ts
@@ -0,0 +1,38 @@
+import type { User } from "../types";
+
+export const user: User[] = [
+    {
+        id: "1",
+        name: "John",
+        lastname: "Doe",
+        phone_number: "123456789",
+        email: "johnd@example.com",
+        address: "Calle 123"
+    },
+    {
+        id: "2",
+        name: "Luis",
+        lastname: "Perez",
+        phone_number: "123456789",
+        email: "luisp@example.com",
+        address: "Calle 123"
+    },
+    {
+        id: "3",
+        name: "Maria",
+        lastname: "Gomez",
+        phone_number: "123456789",
+        email: "mariag@example.com",
+        address: "Calle 123"
+    },
+    {
+        id: "4",
+        name: "Ana",
+        lastname: "Rodriguez",
+        phone_number: "123456789",
+        email: "anar@example.com",
+        address: "Calle 123"
+    },
+]
+
+
